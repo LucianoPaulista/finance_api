f3823daa546a7ba02495d06b6b7a9eb578e107c8
LucianoPaulista <lucianodiaspaulista@gmail.com>
2021-08-12 05:44:51 -0300

    feat: adicionado resources para participantes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   resources :participantes
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+    get 'participantes' ,to: 'participantes#index'
+    get 'participantes/:id' ,to: 'participantes#show'
+    post 'participantes' ,to: 'participantes#create'
+    put 'participantes/:id' ,to: 'participantes#update'
+    delete 'participantes/:id' ,to: 'participantes#destroy'
 end
