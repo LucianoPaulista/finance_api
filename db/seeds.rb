e416f6f0011f704ffc15ce6e86a9804cfda54114
LucianoPaulista <lucianodiaspaulista@gmail.com>
2021-08-13 06:45:10 -0300

    feature: implementado seed para endereço

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,3 +2,8 @@ for i in 1..9 do
   Participante.create(tipo_pessoa: 0, nome: "Luciano Dias Paulista", cnpj_cpf: "123456789", inscricao_rg: "987654321")
   Participante.create(tipo_pessoa: 1, nome: "Luciano Dias Paulista", cnpj_cpf: "123456789", inscricao_rg: "987654321")
 end
+
+for x in 1..3 do
+  Endereco.create(cep: "16260000", endereco: "Rua Maestro Antônio Guerra", endereco_nro: "1039", complemento: "casa",
+                  bairro: "Gentil Bernardo do Prado", cidade: "Coroados", estado: "SP", participante_id: 1)
+end
