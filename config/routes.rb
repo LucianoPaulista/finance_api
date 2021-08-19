11fd8c871b7a2c871bbd345089abb7d9931ac5ef
LucianoPaulista <lucianodiaspaulista@gmail.com>
2021-08-18 22:15:19 -0300

    feature: added routes for invoice types

--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,11 @@ Rails.application.routes.draw do
     post 'addresses' ,to: 'addresses#create'
     put 'addresses/:id' ,to: 'addresses#update'
     delete 'addresses/:id' ,to: 'addresses#destroy'
+
+  resources :invoice_types
+    get 'invoice_types' ,to: 'invoice_types#index'
+    get 'invoice_types/:id' ,to: 'invoice_types#show'
+    post 'invoice_types' ,to: 'invoice_types#create'
+    put 'invoice_types/:id' ,to: 'invoice_types#update'
+    delete 'invoice_types/:id' ,to: 'invoice_types#destroy'
 end
