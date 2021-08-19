b32b1227f8ee2a17e125421b4fc1fd986becbd63
LucianoPaulista <lucianodiaspaulista@gmail.com>
2021-08-18 23:26:36 -0300

    feature: added schema for low_types

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_18_092346) do
+ActiveRecord::Schema.define(version: 2021_08_19_012408) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,12 @@ ActiveRecord::Schema.define(version: 2021_08_18_092346) do
   end
 
   create_table "invoice_types", force: :cascade do |t|
+    t.string "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "low_types", force: :cascade do |t|
     t.string "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
