99a66493088f1fb9135b86f2cfb61adf7bc9c529
LucianoPaulista <lucianodiaspaulista@gmail.com>
2021-08-19 22:46:30 -0300

    feature: added seed for controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,11 +3,13 @@
   3.times do
     @address =  Address.create(street: "Rua Maestro Antônio Guerra", zip_code: "16260000", address_number: "39", complement: "casa", state: "SP", city: "Coroados", neighborhood: "Gentil Bernardes", participant: participant)
   end
-  InvoiceType.create(description: "Contas a Pagar");
+  invoice_type = InvoiceType.create(description: "Contas a Pagar");
   InvoiceType.create(description: "Contas a Receber");
   InvoiceType.create(description: "Despesas a Pagar");
 
   LowType.create(description: "Baixa Total")
   LowType.create(description: "Baixa Parcial")
+  5.times{
+    Invoice.create(issuance_date: "2021-08-19", observation: "Correspondente  a Internet", total_invoice: 1000.00,  participant: participant, invoice_type: invoice_type )
+  }
 end
-
