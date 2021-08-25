51ceb5a5201c51681af146c21bb74fe586d1e82a
LucianoPaulista <lucianodiaspaulista@gmail.com>
2021-08-24 22:19:03 -0300

    test: implemented tests for participant with factory_bot

--- spec/models/participant_spec.rb
+++ spec/models/participant_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Participant, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'create participant' do
+    participant = build(:participant, name: 'Luciano Paulista', surname: 'Paulista')
+    expect(participant.name).to eq 'Luciano Paulista'
+  end
 end
